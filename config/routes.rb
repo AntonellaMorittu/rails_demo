f6c71f42e983797773ce07004b61fdcae156918d
Antonella Morittu <morittuantonella@gmail.com>
2018-02-24 18:28:11 +0100

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'articles/index'
   root controller: :articles, action: :index
-  resources :articles, except: [:destroy]
+  resources :articles, except: [:destroy] do
   resources :comments, only: :create
+  end
 end
